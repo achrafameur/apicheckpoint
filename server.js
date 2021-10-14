4556fe9ed864266b2276d3727267de87030bb000
Achraf Ameur <72624148+achrafameur@users.noreply.github.com>
2021-10-14 16:40:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const User = require("./models/User")
+const User = require("./models/User");
+
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, {
