23657955d64f00f6821891084d6732edf960a027
Achraf Ameur <72624148+achrafameur@users.noreply.github.com>
2021-10-14 15:54:42 +0100

    Update cnx.js

--- connection/cnx.js
+++ connection/cnx.js
@@ -1,13 +1,13 @@
-const mongoose = require ('mongoose');
+// const mongoose = require ('mongoose');
 
-mongoose.connect('mongodb://localhost:27017',{
-    dbName:'apicheckpoint',
-    useNewUrlParser:true,
-    useUnifiedTopology: true
-})
-.then(()=>{
-    console.log('mongodb connected.')
-})
-.catch(()=>{
-    console.error('mongodb not connected.')
-})
+// mongoose.connect('mongodb://localhost:27017',{
+//     dbName:'apicheckpoint',
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true
+// })
+// .then(()=>{
+//     console.log('mongodb connected.')
+// })
+// .catch(()=>{
+//     console.error('mongodb not connected.')
+// })
